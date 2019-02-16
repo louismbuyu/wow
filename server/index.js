b932734ca5560cab2246d991b1001dfdbe3b615d
Louis Mbuyu <louismbuyu17@gmail.com>
2019-02-16 02:18:46 -0600

    changed tests

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req,res) => {
-    return res.json({success: true, message: "Hello World!"});
+    return res.json({success: true, message: "Hello World!", exit:1});
 });
 
 app.get('/heartbeat', (req,res) => {
-    return res.json({success: true, message: "Beating!"});
+    return res.json({success: true, message: "Beating!", exit:1});
 });
 
 app.get('/wow', (req,res) => {
-    return res.json({success: true, message: "WOW!"});
+    return res.json({success: true, message: "WOW!", exit:1});
 });
 
 const PORT = process.env.PORT | 5000;

ffd913cee3c540a6f915a8ef7985719f54b2ce06
Louis Mbuyu <louismbuyu17@gmail.com>
2019-02-18 21:56:00 -0600

    updated cors setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.use(function (req,res,next) {
+    if (req.headers.origin === 'https://simplegone.com' || req.headers.origin === 'https://www.simplegone.com'){
+        res.header('Access-Control-Allow-Origin',req.headers.origin);
+        res.header('Access-Control-Allow-Methods',"GET,PUT,POST,DELETE");
+        res.header('Access-Control-Allow-Headers',"Content-type");
+    }
+    next();
+});
+
 app.get('/', (req,res) => {
     return res.json({success: true, message: "Hello World!", exit:0});
 });
